899819f5530988a426b78ae2b6424082789e5a40
Alphonsus Adu-Bredu <alphonsusbq436@gmail.com>
2022-06-05 17:32:44 +0000

    cleaned up dependencies

--- src/Fabrics.jl
+++ src/Fabrics.jl
@@ -5,12 +5,7 @@ using DataStructures: CircularBuffer
 using ForwardDiff
 using LinearAlgebra
 using StaticArrays
-using Colors
-using PyCall
-using Dojo 
-# using Pkg
-ENV["PYTHON"]="/home/alphonsus/anaconda3/envs/digit/bin/python"
-# Pkg.build("PyCall")
+using Colors 
 
 include("systems/point_mass/types.jl")
 include("systems/point_mass/visualize.jl")
@@ -31,13 +26,7 @@ include("systems/multi_planar_arm/fabric.jl")
 include("systems/picklerick/types.jl")
 include("systems/picklerick/visualize.jl")
 include("systems/picklerick/step.jl")
-include("systems/picklerick/fabric.jl")
-
-# include("systems/digit/types.jl")
-# include("systems/digit/visualize.jl")
-# include("systems/digit/step.jl")
-# include("systems/digit/fabric.jl")
-# include("systems/digit/visualization/initialize.jl")
+include("systems/picklerick/fabric.jl") 
 
 export visualize_system!,
        step!,
@@ -56,15 +45,6 @@ export MultiPlanarArm,
        multiplanar_arm_fabric_solve
 
 export PickleRick,
-       picklerick_fabric_solve
-
-# export Digit, 
-#        digit_fabric_solve,
-#        init_digit_server,
-#        get_observation,
-#        send_command,
-#        mirror_joint_configurations!,
-#        get_digit,
-#        initialize_digit!
+       picklerick_fabric_solve 
 
 end
